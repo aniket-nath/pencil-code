a6d1d55b7fd82f5c6196df7c341526b5d6f338c7
Touko Puro <ToxPuro>
2026-03-27 19:00:08 +0200

    calculate the maximal dt1 on the Astaroth side when using GPUs to have minimal amount of synchronizations

--- src/sub.f90
+++ src/sub.f90
@@ -4118,7 +4118,8 @@ module Sub
       dt1_local=dt1_
       ! Timestep growth limiter
       if (ddt > 0.) dt1_local=max(dt1_local,dt1_last)
-      call mpiallreduce_max(dt1_local,dt1,MPI_COMM_PENCIL)
+!TP: when using the GPU the timestep has already been reduced across ranks
+      if(.not. lgpu) call mpiallreduce_max(dt1_local,dt1,MPI_COMM_PENCIL)
 !
 !  now set the actual time step, based on dt1
 !
