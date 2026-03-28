ea00c41ff67b9f5e1ba976bad25cbd8f064c679c
ToxPuro <touko.puro@aalto.fi>
2026-03-28 17:04:58 +0100

    add missing use

--- src/eos_chemistry_simple.f90
+++ src/eos_chemistry_simple.f90
@@ -1126,6 +1126,9 @@ module EquationOfState
 !***********************************************************************
     subroutine pushpars2c(p_par)
 
+    use Syscalls, only: copy_addr
+    use General,  only: string_to_enum
+
     integer, parameter :: n_pars=100
     integer(KIND=ikind8), dimension(n_pars) :: p_par
 
