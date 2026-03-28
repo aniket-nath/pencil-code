c7471c3f84a2c35c4bfb74429db6c177537df3a8
Axel Brandenburg <brandenb@nordita.org>
2026-03-28 02:33:16 +0000

    param_io.f90: commented out call read_namelist(read_forcing_run_pars,'forcing',lforcing, loptional) and call write_forcing_run_pars(unit) forcing.h: !AB: commented out ! public :: read_forcing_run_pars,  write_forcing_run_pars

--- src/forcing.h
+++ src/forcing.h
@@ -2,7 +2,8 @@
   private
 
   public :: register_forcing, initialize_forcing
-  public :: read_forcing_run_pars,  write_forcing_run_pars
+!AB: commented out
+! public :: read_forcing_run_pars,  write_forcing_run_pars
   public :: output_persistent_forcing, input_persistent_forcing
   public :: rprint_forcing
   public :: addforce
